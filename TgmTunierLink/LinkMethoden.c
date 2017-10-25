5f8953a2c0a92003bb35785245388fc8bb4036d1
mstocker-tgm <mstocker@student.tgm.ac.at>
2017-10-25 14:19:15 +0200

    version 6 folgen Methode geschrieben

--- TgmTunierLink/LinkMethoden.c
+++ TgmTunierLink/LinkMethoden.c
@@ -1,3 +1,18 @@
+void folgen(){
+	while(analog(0) > 800){
+		motor(0,90);
+		motor(1,100);
+		msleep(1);
+	}
+	while(analog(1) > 800){
+		motor(0,100);
+		motor(1,90);
+		msleep(1);
+	}
+	motor(0,100);
+	motor(1,100);
+	msleep(1);
+}
 
 void gerade(int sec){ 		//getestet und funktioniert
 	motor(0,100);
@@ -21,43 +36,4 @@ void links(int sec){		//getestet und funktioniert
 	motor(0,-100);
 	motor(1,100);
 	msleep(sec);
-}
-
-void umdrehen(){			//getestet und funktioniert
-	motor(0,-100);
-	motor(1,100);
-	msleep(4000);
-}
-
-void nachOben(){			//nicht getestet
-	/**
-	while(!Link oben){			Wie machen, dass der Link weiﬂ
-		msleep(1);				ob er oben ist?
-		while(sensor rechts = schwarz){
-			motor(0,90);
-			motor(1,100);
-			msleep(1);
-		}
-		while(sensor links = schwarz){
-			motor(0,100);
-			motor(1,90);
-			msleep(1);
-		}
-	}*/
-}	
-
-void zumLPlanter(){			//nicht getestet
-	while(/*Link nicht bei Planter*/1){
-		while(analog(1) < 0){		//Wert f¸r schwarze Linie herausfinden
-			motor(0,90);
-			motor(1,100);
-			msleep(1);
-		}
-		while(analog(0) < 0){		//Wert f¸r schwarze Linie herausfinden
-			motor(0,100);
-			motor(1,90);
-			msleep(1);
-		}
-	msleep(1);
-	}
 }
