74fd2673a1950dcd2999f8f70b59877c5554d687
mstocker-tgm <mstocker@student.tgm.ac.at>
2017-10-29 08:29:34 +0100

    Modellbaumesse

--- TgmTunierLink/LinkMain.c
+++ TgmTunierLink/LinkMain.c
@@ -1,10 +1,17 @@
 int main(){
-	
-	shut_down_in(119);
-	wait_for_light(1);
-
-	while(analog(1)<450){
-		folgen();
+	while(1){
+		while(analog(2) > 600){
+				motor(0,-30);
+				motor(1,100);
+				msleep(1);
+			}
+			while(analog(3) > 600){
+				motor(0,100);
+				motor(1,-100);
+				msleep(1);
+			}
+		motor(0,100);
+		motor(1,50);
+		msleep(1);
 	}
-	
 }
